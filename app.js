f1d0a9999dc4450f4d264f98ebe5f4a02f5bce28
tushartatke7-art <tushartatke7@gmail.com>
2026-03-01 14:41:36 +0000

    Add Mini E-Commerce Express application

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const products = [
+  { id: 1, name: "Laptop", price: 50000 },
+  { id: 2, name: "Mobile", price: 20000 },
+  { id: 3, name: "Headphones", price: 2000 }
+];
+
+app.get('/', (req, res) => {
+  res.send("🛒 Welcome to Tushar's Mini E-Commerce Store");
+});
+
+app.get('/products', (req, res) => {
+  res.json(products);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
